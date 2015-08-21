dfe5cf04a6395e83806588acd2f358b2acd794b4
yvdorofeev <yvdorofeev@gmail.com>
2015-08-21 13:57:19 -0700

    updates to parser

--- meteor-app/client/views/posts/page-new.js
+++ meteor-app/client/views/posts/page-new.js
@@ -3,13 +3,13 @@ Template.postsNew.created = function(){
   this.data ? _.extend(this.data, data) : _.extend({}, data);
 
   //temp
-  /*Meteor.call('parseUrl', 'http://sacramento.craigslist.org/tag/5171787559.html', function(error, response){
+  Meteor.call('parseUrl', 'https://en.wikipedia.org/wiki/NASA', function(error, response){
     if (response.success) {
       console.log('success\ntitle: ' + response.title + '\nurl: ' + response.imageUrl + '\ncontent: ' + response.content);
     } else {
       console.log('failed');
     }
-  });*/
+  });
 
 };
 Template.postsNew.events({
