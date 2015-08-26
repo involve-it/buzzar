1d736be85923887f05fab6546b69b8448b967ddc
yvdorofeev <yvdorofeev@gmail.com>
2015-08-26 16:06:33 -0700

    improved filter performance for proximityHandler

--- meteor-app/client/config/cordova.js
+++ meteor-app/client/config/cordova.js
@@ -4,9 +4,17 @@
 Meteor.startup(function(){
     var watchId = navigator.geolocation.watchPosition(function(position){
         console.log('coords: ' + position.coords.latitude + ', ' + position.coords.longitude);
-        Session.set('currentLocation', position.coords);
-        Meteor.call('getNearbyPosts', Meteor.userId(), position.coords.latitude, position.coords.longitude, function(err, result){
-            console.log(result);
-        });
+        var currentLocation = Session.get('currentLocation');
+        if (!currentLocation || currentLocation.accuracy != position.coords.accuracy || currentLocation.latitude != position.coords.latitude || currentLocation.longitude != position.coords.longitude){
+            Session.set('currentLocation', {
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude,
+                accuracy: position.coords.accuracy
+            });
+            Meteor.call('getNearbyPosts', Meteor.userId(), position.coords.latitude, position.coords.longitude, function(err, result){
+                //update ui
+                console.log(result);
+            });
+        }
     });
 });
