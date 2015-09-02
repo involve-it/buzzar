5d169caa7cf9234582399c3ab377461fce47c76b
ashot <arutune@gmail.com>
2015-09-01 22:27:32 -0700

    Added FB sdk, implemented share button in share-page.

---
+++ meteor-app/client/00_inits/need/put/really/very/deep/fb.js
@@ -0,0 +1,14 @@
+/**
+ * Created by root on 9/1/15.
+ */
+if(Meteor.isClient) {
+  window.fbAsyncInit = function() {
+    debugger;
+    FB.init({
+      appId      : '1025008177543236',
+      status     : true,
+      xfbml      : true,
+      version    : 'v2.4'
+    });
+  };
+}
