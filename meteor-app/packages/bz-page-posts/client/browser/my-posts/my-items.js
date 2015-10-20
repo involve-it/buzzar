b18ef3863322954495a47b88e1549522b55643b0
douson24 <infpartner@gmail.com>
2015-10-20 16:40:17 +0300

    Added my posts template

--- meteor-app/packages/bz-page-posts/client/browser/my-posts/my-items.js
+++ meteor-app/packages/bz-page-posts/client/browser/my-posts/my-items.js
@@ -27,7 +27,16 @@ Template.myItems.helpers({
   },
   inactivePosts: function () {
     var posts = bz.cols.posts.find({userId: Meteor.userId()}).fetch();
+    
     return posts;
+  },
+  bzCountActivePosts: function() {
+    var postsCount = bz.cols.posts.find({userId: Meteor.userId()}).count();
+    return postsCount || '0';
+  },
+  bzCountInactivePosts: function() {
+    var postsCount;
+    return postsCount || '0';
   }
 });
 
@@ -65,6 +74,46 @@ Template.onePostRowItem.helpers({
   }
 });
 
+
+
+
+/* 
+* Template for owner posts
+*/
+
+Template.onePostRowItemOwner.helpers({
+  getPhotoUrl: function () {
+    var photo = bz.cols.posts.findOne({_id: this._id}),
+        photoId = photo.details.photos && photo.details.photos[0] || undefined;
+
+    if (photoId) {
+      var image = bz.cols.images.findOne({_id: photoId});
+    }
+
+    return image;
+
+  },
+  getPrice: function () {},
+  categoryType: function() {
+    return bz.cols.posts.find({_id: this._id}).fetch()[0].type;
+  },
+  getAvatarImg: function () {
+    var ret ='';
+
+    if(this.userId && Meteor.users.findOne(this.userId) && Meteor.users.findOne(this.userId).profile.image) {
+      return ret = Meteor.users.findOne(this.userId).profile.image;
+    } else {
+      return '/img/content/avatars/avatar-no.png';
+    }
+  },
+  getUserName: function() {
+    var ret = '';
+    if(this.userId && Meteor.users.findOne(this.userId)) {
+      ret = Meteor.users.findOne(this.userId).username.toCapitalCase();
+    }
+    return ret;
+  }
+});
 
 
 
