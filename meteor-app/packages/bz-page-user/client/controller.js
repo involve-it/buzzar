2d0a8884d4624311c1a8621917a71d6378785928
arutu_000 <arutune@gmail.com>
2016-02-06 22:02:20 -0800

    image upload control - profile image is fixed too

--- meteor-app/packages/bz-page-user/client/controller.js
+++ meteor-app/packages/bz-page-user/client/controller.js
@@ -9,7 +9,6 @@ Meteor.startup(function () {
     //var img = Session.get('bz.user.profileImgUploaded');
     var img = avatarThumbnailReactive.get() && avatarThumbnailReactive.get()[0];
     console.log(img);
-    debugger;
     if (img && img !== '') {
       Meteor.users.update(Meteor.userId(), { $set: {'profile.image': {
         data: img.data
