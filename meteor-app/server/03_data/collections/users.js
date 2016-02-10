7fbe1185284099875b983b8be2dabe611ecee27b
xvolkx48 <xvolkx48@gmail.com>
2016-02-10 10:44:26 +0300

    Fix users publish

--- meteor-app/server/03_data/collections/users.js
+++ meteor-app/server/03_data/collections/users.js
@@ -2,11 +2,15 @@
  * Created by Ashot on 9/27/15.
  */
 Meteor.publish('bz.users.all', function(){
-  return Meteor.users.find();
-})
-/*Meteor.publish('bz.users.all', function(){
-  return Meteor.users.find();
+  return Meteor.users.find({}, {fields: {
+    '_id':1,
+    'username':1,
+    'createdAt':1,
+    'profile':1,
+    'online':1
+  }});
+});
+
+/*Meteor.publish('bz.users.byId', function(userId){
+  return Meteor.users.findOne(userId);
 })*/
-Meteor.publish('bz.users.byId', function(userId){
-  return Meteor.users.findOne( userId )
-})
