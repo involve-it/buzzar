9961da4961f2f4f2dedd7b32e11ea0b54862fa1e
xvolkx48 <xvolkx48@gmail.com>
2016-04-27 23:20:37 +0300

    issue 192:  search test function(search by tags)

--- meteor-app/packages/bz-page-tags/client/browser/page-tags.js
+++ meteor-app/packages/bz-page-tags/client/browser/page-tags.js
@@ -1,3 +1,8 @@
 /**
  * Created by xvolkx48 on 18.04.2016.
  */
+
+Template.pageTags.onCreated(function(){
+  Session.set('bz.search.tag-category',"");
+  Session.set('bz.search.tag-related',[])
+});
