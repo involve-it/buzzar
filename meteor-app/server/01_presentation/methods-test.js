0df729c28a51ec0d2c1f715afac7fe13de56b62d
xvolkx48 <xvolkx48@gmail.com>
2016-04-30 05:26:14 +0300

    userHandler fix small bug

--- meteor-app/server/01_presentation/methods-test.js
+++ meteor-app/server/01_presentation/methods-test.js
@@ -1,15 +1,8 @@
 /**
  * Created by xvolkx48 on 29.04.2016.
  */
-
-function getUser(userId) {
-  return bz.bus.usersHandler.getUser(userId, Meteor.userId());
-}
-function editUser() {
-  var ret = bz.bus.usersHandler.editUser();
-  return ret;
-}
-function addUser() {
-  var ret = bz.bus.usersHandler.addUser();
-  return ret;
-}
+Meteor.methods({
+  getUser:function(userId) {
+    return bz.bus.usersHandler.getUser(userId, Meteor.userId());
+  }
+});
