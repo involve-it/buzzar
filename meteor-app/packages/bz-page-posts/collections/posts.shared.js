f76eddc29d82e5a833ae86a7e98b465aa833436d
c_aarutyunyan <c_aarutyunyan@groupon.com>
2016-07-15 22:40:33 -0700

    small change

--- meteor-app/packages/bz-page-posts/collections/posts.shared.js
+++ meteor-app/packages/bz-page-posts/collections/posts.shared.js
@@ -4,12 +4,12 @@
 bz.cols.posts = new Mongo.Collection('posts');
 Ground.Collection(bz.cols.posts);
 var postsColl = bz.cols.posts;
-
-postsColl.helpers({
+bz.cols.posts.bzHelpers = {
   _hasLivePresence: bz.help.posts.hasLivePresence,
   _getDistanceToCurrentLocation: bz.help.posts.getDistanceToCurrentLocation,
   _getDistanceToCurrentLocationNumber: bz.help.posts.getDistanceToCurrentLocationNumber,
   //_getLikesAmount: bz.help.posts.getLikesAmount,
   _getDistanceQualifier: bz.help.posts.getDistanceQualifier,
   _getImagesObjects: bz.help.posts.getImagesObjects
-});
+}
+postsColl.helpers(bz.cols.posts.bzHelpers);
