c5bfb90b807bd9ded3399bd3f266ef1fe0713a41
xvolkx48 <xvolkx48@gmail.com>
2016-10-10 10:14:28 +0300

    add isIframe param for render page

--- meteor-app/packages/bz-api/shared/lib/01_inits/02_helpers.js
+++ meteor-app/packages/bz-api/shared/lib/01_inits/02_helpers.js
@@ -156,3 +156,12 @@ bz.error = function(error){
   bz.help.logError(error);
   Meteor.error(error);
 }
+
+bz.help.getParamURL = function () {
+  var queries = {};
+  $.each(document.location.search.substr(1).split('&'),function(c,q){
+    var i = q.split('=');
+    queries[i[0].toString()] = i[1].toString();
+  });
+  return queries;
+}
