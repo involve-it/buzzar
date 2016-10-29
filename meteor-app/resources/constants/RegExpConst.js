1ddccee4b72f26ae174179f1e1ff78481b34e60b
yvdorofeev <yvdorofeev@gmail.com>
2016-10-29 11:33:22 -0700

    removed profile image URL validation

--- meteor-app/resources/constants/RegExpConst.js
+++ meteor-app/resources/constants/RegExpConst.js
@@ -3,5 +3,7 @@
  */
 bz.help.makeNamespace('bz.const.RegExp');
 bz.const.RegExp.emaileRegEx =/[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}/i; // this is total absolute bullshitting!
-bz.const.RegExp.imageUrlRegEx =/^(https:\/\/s3.amazonaws.com\/shiners\/v1.0\/public\/images\/)[A-Z0-9.-]+/i;
+//bz.const.RegExp.imageUrlRegEx =/^(https:\/\/s3.amazonaws.com\/shiners\/v[0-9].[0-9]\/public\/images\/)[A-Z0-9.-]+/i;
+//bz.const.RegExp.imageUrlRegEx2 =/^(https:\/\/buzzar.s3-us-west-1.amazonaws.com\/v[0-9].[0-9]\/public\/images\/)[A-Z0-9.-]+/i;
 // bz.const.RegExp.imageUrlRegEx =/^(https:\/\/buzzar.s3-us-west-1.amazonaws.com\/v1.0\/public\/images\/)[A-Z0-9.-]+/i;
+//https://buzzar.s3-us-west-1.amazonaws.com/v1.0/public/images/
